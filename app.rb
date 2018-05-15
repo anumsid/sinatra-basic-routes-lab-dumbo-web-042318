8bd135d1d4ed0dad8dd7d7bf3cb2ed94a35e49f3
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-05-15 10:09:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is "
+    # binding.pry
+  end
+
+  get '/hometown' do
+    "My hometown is "
+  end
+
+  get '/favorite-song' do
+    "My favorite song is "
+  end
 end
